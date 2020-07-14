22f75cffc04ed55dc20a26b3c77a827839a2a042
Heather Ransome <siobahnv@gmail.com>
2020-07-14 16:20:10 -0700

    Setup model.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Example(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
