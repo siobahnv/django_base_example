085e451bc1ab76d94b33e5e446cda926d1d9fe60
Heather Ransome <siobahnv@gmail.com>
2020-07-14 16:40:59 -0700

    Woo got a base working./manage.py runserver

--- app/views.py
+++ app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def example_list(request):
-    return render(request, 'example_list.html', {})
+    return render(request, 'app/example_list.html', {})
