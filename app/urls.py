2e7161af5db368dff29660c558f6c256912e7504
Heather Ransome <siobahnv@gmail.com>
2020-07-14 17:10:27 -0700

    Added nav and linked things.

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "app"
 urlpatterns = [
     path('', views.example_list, name='example_list'),
+    path('home/', views.home, name='home'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'app/home.html', {})
+
 def example_list(request):
     return render(request, 'app/example_list.html', {})
